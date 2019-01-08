57758219d284cb6686cc82088d66cee2dc352951
Vantorre Fabien <vantorre@b13p11.fil.univ-lille1.fr>
2019-01-08 16:01:46 +0100

    ex2

---
+++ src/timers.js
@@ -0,0 +1,20 @@
+const john = 'John';
+const arya = 'Arya';
+const sensa = 'Sensa';
+const say = hero => sentence => console.log(`${hero} says : ${sentence}`);
+const johnSay = say(john); // may be used this way setTimeout(johnSay, 1000, 'hello');
+const aryaSay = say(arya);
+const sensaSay = say(sensa);
+
+
+const sensaInterval = setInterval(() => sensaSay("For the north"), 1000);
+setTimeout(() => clearInterval(sensaInterval), 10000);
+
+
+setTimeout(() => {
+        johnSay("hello Ladies Stark")
+        setImmediate(() => aryaSay("thank you for needle"));
+    }
+    , 2000
+);
+
